1fcf9574ec2af314635ba5462332231581d2b2d4
kumailZaidi12 <zaidikumail12@gmail.com>
2020-11-02 00:03:40 +0530

    set the handlebars

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
-
+const app = express()
 //Load config
 dotenv.config({path: './config/config.env' })
 
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+  
+
 connectDB()
 
-const app = express()
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
